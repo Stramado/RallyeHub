66f3de8387ab67420a685a7c102739bfb7fd43c2
Stramado <stramado2.0@gmail.com>
2026-01-18 21:44:50 +0100

    add begging of a categorieBarSample function

--- watch.php
+++ watch.php
@@ -6,10 +6,14 @@ include "./src/php/functions.php";
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Essai Porsche 911 - CarVideos</title>
-	<link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style2.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta name="language" content="fr">
+    <meta name="description" content="Profitez de regarder les vidéos de vos pilotes de rallye préférés, avec vos amis, votre famille et le monde entier sur RallyHub">
+    <meta name="keywords" content="vidéo, partage, rallye, gratuit, visionnage, social">
+    <title>RallyeHub - Watch a Video</title>
+    <link rel="stylesheet" href="/static/stylesheets/main.css">
+    <link rel="stylesheet" href="/static/stylesheets/watch.css">
+    <link rel="icon" type="image/x-icon" href="/static/img/favicon.ico">
     <script src="https://unpkg.com/lucide@latest"></script>
 </head>
 <body>
